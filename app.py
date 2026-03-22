453e744bd270b53ca49b5001d7b2e0729f7d1988
Yael Livnat Zaidman <101816560+Flyingy37@users.noreply.github.com>
2026-03-22 19:17:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,9 @@ with tab1:
         st.error("paste.txt לא נמצא")
 
 with tab2:
+    st.subheader("קובץ תרגול")
     st.markdown('<div class="card-box">', unsafe_allow_html=True)
+    st.markdown("#### קובץ תרגול")
     st.markdown("גרסה לעבודה עצמאית במהלך התרגול.")
     if student_file.exists():
         st.download_button(
@@ -101,6 +103,7 @@ with tab2:
             file_name=student_file.name,
             mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
             use_container_width=True
+        )
     else:
         st.error("קובץ התרגול לא נמצא")
     st.markdown('</div>', unsafe_allow_html=True)
